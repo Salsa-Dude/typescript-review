fe8d37e64d1be168d747351fc5fd00ca85a9fd18
joseph <arias.joseph.a@gmail.com>
2022-01-16 16:09:33 -0500

    object-types

---
+++ object-types/app.js
@@ -0,0 +1,9 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+var person = {
+    name: "joseph",
+    age: 25
+};
+console.log(person.name);
---
+++ object-types/app.ts
@@ -0,0 +1,12 @@
+
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+  
+const person = {
+  name: "joseph",
+  age: 25
+}
+
+console.log(person.name);
