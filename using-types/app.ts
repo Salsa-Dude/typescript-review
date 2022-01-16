46e1010ca24e07617bceae940f521194686e6753
joseph <arias.joseph.a@gmail.com>
2022-01-16 15:48:34 -0500

    Type assignment & Type Inference

--- using-types/app.ts
+++ using-types/app.ts
@@ -1,9 +1,18 @@
-/* The key difference is: JavaScript uses "dynamic types" (resolved at runtime). TypeScript uses "static types" (set during development) */
+/* The key difference is: JavaScript uses "dynamic types" (resolved at runtime). TypeScript uses "static types" (set during development)
+
+- Types allow you to detect errors early and avoid some runtime errors
+
+- TS types are checked during compilation, JS types during runtime.
+
+*/
 
 function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
+// * Type Inference *
+// let num1: number;
+// num1 = 5;
 const number1 = 5;
 const number2 = 2.8;
 
