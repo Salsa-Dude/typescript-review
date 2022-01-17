8e70e3af365f549137a758c9d9e3df7b3e182fa0
joseph <arias.joseph.a@gmail.com>
2022-01-17 12:48:12 -0500

    Arrays Types

--- object-types/app.ts
+++ object-types/app.ts
@@ -3,10 +3,14 @@
 //   name: string;
 //   age: number;
 // } = {
-  
+
 const person = {
   name: "joseph",
-  age: 25
+  age: 25,
+  hobbies: ['Sports', 'Dancing']
 }
+
+// explict
+let favoriteActivitites: string[]; 
 
 console.log(person.name);
