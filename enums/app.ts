80bf7445e9ab00cafbc17a748987dadf35051c30
joseph <arias.joseph.a@gmail.com>
2022-01-17 13:53:04 -0500

    Working with Enums

---
+++ enums/app.ts
@@ -0,0 +1,12 @@
+
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+
+const person = {
+  name: "joseph",
+  age: 25,
+  hobbies: ['Sports', 'Dancing'],
+  role: Role.ADMIN
+};
+
+console.log(person.role) // 0
+
