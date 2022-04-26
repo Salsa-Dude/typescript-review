95c68c99c9ba8cc5b1b3bdd8e861d2fff9651d5b
Joseph Arias <jaria001@odu.edu>
2022-04-25 20:48:38 -0400

    added enum description

--- enums/app.ts
+++ enums/app.ts
@@ -1,3 +1,8 @@
+/*
+  Enum is a enumerated list which makes it easier to handle named sets of constants.
+  By default an enum is number based, starting at zero,
+  and each option is assigned an increment by one.
+*/
 
 enum Role { ADMIN, READ_ONLY, AUTHOR };
 
