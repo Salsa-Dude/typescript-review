650f6022078f8fd1210ffac0568afd1fb731ca47
Joseph Arias <jaria001@odu.edu>
2022-05-16 06:13:26 -0400

    Function Return Types & 'void'

---
+++ function-return-types & void/app.ts
@@ -0,0 +1,10 @@
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+// void function
+function printResults(num: number) : void {
+  console.log('Result ' + num);
+}
+
+printResults(add(5, 12));
